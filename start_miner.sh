77b35b4c137ecea87bf90099c61b849a3a44d55e
COAmining <68738548+COAmining@users.noreply.github.com>
2020-11-14 12:40:42 +0900

    Update start_miner.sh

--- start_miner.sh
+++ start_miner.sh
@@ -1,1 +1,1 @@
-/home/coa/cob/PhoenixMiner_5.0e_Linux/PhoenixMiner -epool asia1.ethermine.org:14444 -ewal 0xb3d3478c42a45f7d06cc7f5dc241b57b7a7ef5bb.0700 -epsw x -esm 1 -allpools 1 -r 1 -dbg -1 -log 0 -amd -proto 3
+ /home/coa/cob/PhoenixMiner_5.0e_Linux/PhoenixMiner -epool asia1.ethermine.org:14444 -ewal 0x6ab1e8041ae57c20e20529a6ab763c299a0d2bbe.0700 -epsw x -esm 1 -allpools 1 -r 1 -dbg -1 -log 0 -amd -proto 3
